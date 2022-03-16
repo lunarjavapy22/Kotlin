0bb78c609ac3aec8b8aeeaddb576709eca09cedf
Selim Özen <96844447+mselimozen@users.noreply.github.com>
2022-03-16 16:14:19 +0300

    Create Avengers.kt

---
+++ ClassLessons/Avengers.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin2
+
+class Avengers {
+    //Özelliklerini (property) tanımlıyoruz.
+    var heroType = ""
+    var heroAge = 0
+    var heroLevel = ""
+    var heroName = ""
+}
