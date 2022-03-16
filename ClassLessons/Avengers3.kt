857cacdea3deaa728340bfc0d8042c4f30a3c7db
Selim Özen <96844447+mselimozen@users.noreply.github.com>
2022-03-16 17:48:22 +0300

    Create Avengers3.kt

---
+++ ClassLessons/Avengers3.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin2
+//2 şekeilde sınıf tanımlayamıyı gördük ama Artık sadece kotlinle gelen bir tanımlama şekli var.
+class Avengers(var name: String, var age: Int, var type: String, var level: String) {
+    /*
+    //Özelliklerini (property) tanımlıyoruz.
+    var heroType = ""
+    var heroAge = 0
+    var heroLevel = ""
+    var heroName = ""
+
+    //constructor sınıfımızdan bir obje oluşturulduğunda çağırılcak metod anlamına gelmektedir.
+    //Clan code için önemli olabilir.
+    constructor(nameInput: String, ageInput: Int, typeInput: String, levelInput: String){
+        this.heroName = nameInput
+        this.heroAge = ageInput
+        this.heroType = typeInput
+        this.heroLevel = levelInput
+        //this içinde bulunduğu bir üst kod parçacığı olan classlara referansta bulunur.
+    }
+*/
+}
